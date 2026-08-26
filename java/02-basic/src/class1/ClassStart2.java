b0bd6e06c17ec92cf3cb123693b4dd93a8c6966b
pleasebelieveme <cain1227@naver.com>
2026-08-26 23:46:48 +0900

    Update ClassStart2.java

--- java/02-basic/src/class1/ClassStart2.java
+++ java/02-basic/src/class1/ClassStart2.java
@@ -2,7 +2,7 @@ package class1;
 
 public class ClassStart2 {
     static void main() {
-        // 배열로 해결해보자!
+        // ClassStart1의 문제를 배열로 해결해보자!
         String[] studentNames = {"학생1", "학생2", "학생3"};
         int[] studentAges = {16, 15, 17};
         int[] studentGrades = {90, 80, 70};
@@ -11,6 +11,6 @@ public class ClassStart2 {
             System.out.println("이름 : " + studentNames[i] + " 나이 : " + studentAges[i] + " 성적 : " + studentGrades[i]);
         }
 
-        // 학생의 데이터가 서로 다른 곳에 위치해서 휴면에러의 가능성이 크다.
+        // 위코드의 문제점 : 각각의 학생데이터가 서로 다른 곳에 위치해서 휴면에러의 가능성이 크다.
     }
 }
