b36e7492fe778bbeb68677407e87ec1db8da98d4
pleasebelieveme <cain1227@naver.com>
2026-09-03 22:24:20 +0900

    실전자바 41강 접근제어자의 이해1

---
+++ java/02-basic/src/access/SpeackerMain.java
@@ -0,0 +1,17 @@
+package access;
+
+public class SpeackerMain {
+    static void main(String[] args) {
+        Speacker speacker = new Speacker(90);
+        speacker.showVolume();
+        speacker.volumeUp();
+        speacker.showVolume();
+        speacker.volumeUp();
+        speacker.showVolume();
+
+        // 필드에 직접 접근
+        System.out.println("----------volume필드에 직접 접근하여 수정-----------");
+        speacker.volume = 200;
+        speacker.showVolume();
+    }
+}
