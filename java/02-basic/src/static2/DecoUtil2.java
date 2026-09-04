d23397566e31348ea7ec53b7a7fd7f464420a370
pleasebelieveme <cain1227@naver.com>
2026-09-05 08:43:27 +0900

    실전자바 56강 static메서드1

---
+++ java/02-basic/src/static2/DecoUtil2.java
@@ -0,0 +1,9 @@
+package static2;
+
+public class DecoUtil2 {
+    // static 키워드 추가!
+    public static String deco(String str) {
+        String result = "*" + str + "*";
+        return result;
+    }
+}
